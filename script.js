98cf2ba8e23af2e62418284d1ce1ca146fe0888b
gibaek-lee <bbsnake10@gmail.com>
2021-08-27 11:02:15 +0900

    fix: 계좌번호 순서 변경

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const isMobile = validIsMobile()
 const NUM_MORE_IMG = 4
 const INFO_AACOUNTS = [
   '계좌번호',
-  '신한은행 110-154-512579 백미하',
-  '카카오뱅크 3333-10-0168181 이기백'
+  '카카오뱅크 3333-10-0168181 이기백',
+  '신한은행 110-154-512579 백미하'
 ]
 
 appendListMorImgs()
